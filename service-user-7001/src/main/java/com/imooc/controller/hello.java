b98c8461b4b900e13b6e41a252c1b6ebec427574
Quan <qwu@ainguraiiot.com>
2025-05-18 10:29:21 +0200

    feature(user): add user config

--- service-user-7001/src/main/java/com/imooc/controller/hello.java
+++ service-user-7001/src/main/java/com/imooc/controller/hello.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.List;
 @RestController
 @Slf4j
 @Tag(name = "示例API", description = "这是一个示例接口")
+@RequestMapping("/u")
 public class hello {
 
     @Autowired
