b7155096e45833aa77716f4c0077a25bea84d791
Quan <qwu@ainguraiiot.com>
2025-05-27 14:10:09 +0200

    feature(seata): add code for manually trigger error to text seata

--- auth-service-8111/src/main/java/com/imooc/service/impl/UsersServiceImpl.java
+++ auth-service-8111/src/main/java/com/imooc/service/impl/UsersServiceImpl.java
@@ -2,6 +2,7 @@ package com.imooc.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.imooc.Users;
 import com.imooc.bo.InitRequest;
 import com.imooc.enums.Sex;
 import com.imooc.enums.ShowWhichName;
@@ -11,16 +12,18 @@ import com.imooc.mapper.UsersMapper;
 import com.imooc.service.UsersService;
 import com.imooc.utils.DesensitizationUtil;
 import com.imooc.utils.LocalDateUtils;
-import com.imooc.Users;
+import io.seata.core.context.RootContext;
+import io.seata.spring.annotation.GlobalTransactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Service
-@Transactional(rollbackFor = Exception.class)
+@Slf4j
+//@Transactional(rollbackFor = Exception.class)
 public class UsersServiceImpl extends ServiceImpl<UsersMapper, Users> implements UsersService {
 
     @Autowired
@@ -32,13 +35,14 @@ public class UsersServiceImpl extends ServiceImpl<UsersMapper, Users> implements
     private static final String USER_FACE1 = "";
 
     @Override
+    @GlobalTransactional // Note: 如果使用多个微服务，不要使用Transactional，请使用seata的GlobalTransactional
     public Users createUser(String mobile) {
         Users user = new Users();
 
         // deal with name
         user.setMobile(mobile);
-        user.setNickname("用户"+ DesensitizationUtil.commonDisplay(mobile));
-        user.setRealName("用户"+ DesensitizationUtil.commonDisplay(mobile));
+        user.setNickname("用户" + DesensitizationUtil.commonDisplay(mobile));
+        user.setRealName("用户" + DesensitizationUtil.commonDisplay(mobile));
         user.setShowWhichName(ShowWhichName.nickname.type);
 
         //
@@ -64,22 +68,29 @@ public class UsersServiceImpl extends ServiceImpl<UsersMapper, Users> implements
         user.setCreatedTime(LocalDateTime.now());
         user.setUpdatedTime(LocalDateTime.now());
 
-//        usersMapper.insert(user);
+        usersMapper.insert(user);
         // 发起一个远程调用,初始化简历，新增一套空的记录
         InitRequest initRequest = new InitRequest();
         initRequest.setUserId(user.getId());
         workMicroServiceFeign.init(initRequest);
 
+        log.info("全局事务 XID: {}", RootContext.getXID());
+
+        try {
+            int a = 1 / 0; // 业务代码
+        } catch (Exception e) {
+            log.error("事务异常，触发回滚", e);
+            throw e; // 必须抛出，Seata 才会回滚
+        }
+
+
         return user;
-
-
-
     }
 
     @Override
     public Users getUserByMobile(String mobile) {
         QueryWrapper<Users> usersQueryWrapper = new QueryWrapper<>();
-        usersQueryWrapper.eq("mobile",mobile).orderByDesc("id").last("limit 1");
+        usersQueryWrapper.eq("mobile", mobile).orderByDesc("id").last("limit 1");
         return usersMapper.selectOne(usersQueryWrapper);
     }
 }
