0b0d46ac13c18b9a9733b1075132fa987a220c13
Quan <qwu@ainguraiiot.com>
2025-05-27 14:08:15 +0200

    feature(seata): config can be used as FeignClient Parameters

--- hire-api/src/main/java/com/imooc/feign/WorkMicroServiceFeign.java
+++ hire-api/src/main/java/com/imooc/feign/WorkMicroServiceFeign.java
@@ -2,14 +2,12 @@ package com.imooc.feign;
 
 import com.imooc.bo.InitRequest;
 import com.imooc.grace.result.GraceJSONResult;
-import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("work-service")
+@FeignClient(value = "work-service") //  configuration = FeignSeataInterceptorConfiguration.class
 public interface WorkMicroServiceFeign {
 
     @PostMapping("/resume/init")
